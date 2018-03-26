c1b2b6a0c22a0cf54c9509e83e10c3a3a1e22645
Dexter Octana <dexter@octlabs.org>
2018-03-26 01:02:53 -0400

    ForwaringUnit started

---
+++ ECE3570Lab2.srcs/sources_1/new/ForwardingUnit.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/26/2018 12:20:38 AM
+// Design Name: 
+// Module Name: ForwardingUnit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ForwardingUnit(
+    input [2:0] reg_dest_addr,
+    input [2:0] reg_source_1,
+    input [2:0] reg_source_2,
+    input [9:0] execute_result
+    
+    );
+endmodule
